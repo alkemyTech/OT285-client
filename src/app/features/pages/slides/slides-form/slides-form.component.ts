3b050e8c958c73301679d9abc6f2c5402f0d53c0
Diego Otranto <diegui98@live.com.ar>
2022-08-19 19:03:49 -0300

    .ts: Form creation, Form getters

--- src/app/features/pages/slides/slides-form/slides-form.component.ts
+++ src/app/features/pages/slides/slides-form/slides-form.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 @Component({
-  selector: 'app-slides-form',
-  templateUrl: './slides-form.component.html',
-  styleUrls: ['./slides-form.component.scss']
+  selector: "app-slides-form",
+  templateUrl: "./slides-form.component.html",
+  styleUrls: ["./slides-form.component.scss"],
 })
 export class SlidesFormComponent implements OnInit {
+  form: FormGroup;
+  public Editor = ClassicEditor;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder // Se inyecta => private http: HttpService
+  ) {
+    this.form = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      image: ["", [Validators.required]],
+      order: ["", [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  sendSlide(event: Event) {}
+
+  //Form Getters
+  get name() {
+    return this.form.get("name");
+  }
+  get description() {
+    return this.form.get("description");
+  }
+  get image() {
+    return this.form.get("image");
+  }
+  get order() {
+    return this.form.get("order");
+  }
 }

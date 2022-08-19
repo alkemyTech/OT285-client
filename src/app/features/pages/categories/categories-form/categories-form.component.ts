36fa35e0fa864a5393918bf47b22a9850ca7a0d8
nahuelba <nahuelbarreiro@gmail.com>
2022-08-19 18:18:42 -0300

    Form categories added. Name and file validators.

--- src/app/features/pages/categories/categories-form/categories-form.component.ts
+++ src/app/features/pages/categories/categories-form/categories-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ImageValidator } from './image.validator';
+
+
 
 @Component({
   selector: 'app-categories-form',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesFormComponent implements OnInit {
 
-  constructor() { }
+  public Editor = ClassicEditor;
+
+  editorConfig = { 
+    placeholder:'Ingrese una descripción...'
+  }
+
+  categoriesForm = this.fb.group({
+    name:['', [Validators.required, Validators.minLength(4), Validators.maxLength(256)]],
+    image:['',[ Validators.required, ImageValidator]],
+    description:['', Validators.required],
+  })
+
+  constructor(
+    private fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
+
+  onFileSelected(event: any): void {
+    let file = event.target.files[0];
+    this.categoriesForm.controls['image'].setValue(file ? file : '');
+  }
+
+  sendForm(){
+    if(this.categoriesForm.valid){
+      console.log(this.categoriesForm.value)
+    }
+  }
+
 
 }

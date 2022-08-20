03466a9460a2fa9b20d15ecd97490c384a9be228
anjoalo1 <anjojalo1@gmail.com>
2022-08-20 16:55:00 -0500

    agregando-validaciones

--- src/app/features/pages/contact/contact.component.ts
+++ src/app/features/pages/contact/contact.component.ts
@@ -12,17 +12,18 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  messageError:string="";
-  errorEmail:string="";
-  errorPassword:string="";
+  nameError:string="Ingresa un nombre valido";
+  emailError:string="Correo no valido";
+  phoneError:string="Ingresa valores numericos";
+  messageError:string="Tienes que dejar un mensaje";
 
 
 
   /*end variables*/
 
 
-  formLogin = new FormGroup({
-    name:   new FormControl('',[Validators.required, Validators.min(2)]),
+  formContact = new FormGroup({
+    name:   new FormControl('',[Validators.required, Validators.min(3), Validators.maxLength(256)], ),
     email : new FormControl('', [Validators.required, Validators.email]),
     phone : new FormControl('', [Validators.required,
       Validators.min(8),
@@ -39,12 +40,10 @@ export class ContactComponent implements OnInit {
 
     addLogin(form:any){
 
-      console.log(this.formLogin.get('name')?.value);
-      console.log(this.formLogin.get('email')?.value);
-      console.log(this.formLogin.get('phone')?.value);
-      console.log(this.formLogin.get('message')?.value);
-
-
+      console.log(this.formContact.get('name')?.value);
+      console.log(this.formContact.get('email')?.value);
+      console.log(this.formContact.get('phone')?.value);
+      console.log(this.formContact.get('message')?.value);
     }
 
 }

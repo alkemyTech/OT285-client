7957c95f3c052549ca60dca089d052728741749a
anjoalo1 <anjojalo1@gmail.com>
2022-08-20 14:38:41 -0500

    formulario-finalizado

--- src/app/features/pages/contact/contact.component.ts
+++ src/app/features/pages/contact/contact.component.ts
@@ -30,14 +30,7 @@ export class ContactComponent implements OnInit {
     message : new FormControl('', [Validators.required])
 
 
-    /* password : new FormControl('', [Validators.required,
-      Validators.min(6),
-      Validators.pattern(/^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/)]),
-                        
-    
-    passwordConfirm: new FormControl('',[Validators.required,
-    Validators.min(6),
-    Validators.pattern(/^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/)]) */
+   
   });
 
 
@@ -51,54 +44,7 @@ export class ContactComponent implements OnInit {
       console.log(this.formLogin.get('phone')?.value);
       console.log(this.formLogin.get('message')?.value);
 
-      /* this.messageError=""
-      let validationPassword:boolean = this.formLogin.get('password')?.value === this.formLogin.get('passwordConfirm')?.value;
-     
-      
-      if(validationPassword){
-        this.myObject[0]=this.formLogin.get('email')?.value;
-      this.myObject[1]=this.formLogin.get('password')?.value;
-      console.log(this.myObject);
-      }
-      else{
-        this.messageError="Passwords do not match";
-      } */
-
-
 
     }
-
-
-    /* validationEmail(): string{
-      if((this.formLogin.get('email')?.touched) && this.formLogin.get('email')?.invalid){
-          this.errorEmail="Wrong Email"; 
-      }else{
-        this.errorEmail="";
-      }
-      return this.errorEmail;
-    }
-
-    validationPassword():string{
-      if((this.formLogin.get('password')?.touched) && this.formLogin.get('password')?.invalid){
-        this.errorPassword="password must have at least 1 lowercase character, 1 uppercase character and a special character #$%&"; 
-    }else{
-      this.errorPassword="";
-    }
-    return this.errorPassword;
-      
-
-    } */
-
-
-
-
-
-
-
-
-
-
-
-
 
 }

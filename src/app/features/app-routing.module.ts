58ecd9c2147be9ac6c8978aba4a10c386685a3b8
nahuelba <nahuelbarreiro@gmail.com>
2022-08-22 15:46:29 -0300

    dashboard component, routing added

--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -2,11 +2,17 @@ import { ActivityFormComponent } from "./pages/activities/activity-form/activity
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 const routes: Routes = [
   { 
     path: "actividades", 
-    component: ActivityFormComponent },
+    component: ActivityFormComponent 
+  },
+  {
+    path:'backoffice',
+    component:DashboardComponent
+  },
   {
     path: "",
     redirectTo: "actividades",

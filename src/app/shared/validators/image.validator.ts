df35f90232f420c85bb8d9d8f41b387a7d8aff1e
Diego Otranto <diegui98@live.com.ar>
2022-08-25 02:12:38 -0300

    ImageValidator + ImageValidator extended

--- src/app/shared/validators/image.validator.ts
+++ src/app/shared/validators/image.validator.ts
@@ -4,5 +4,18 @@ export function ImageValidator(control: AbstractControl): { InvalidType:boolean 
   if (control.value.type == "image/jpeg" || control.value.type == "image/png") {
     return null;
   }
+  if (!control.value.type) {
+    let controlEdgeArray = control.value.split(".");
+    let controlEdgeType =
+      controlEdgeArray[controlEdgeArray.length - 1].toLowerCase();
+    console.log(controlEdgeType);
+    if (
+      controlEdgeType == "jpg" ||
+      controlEdgeType == "png" ||
+      controlEdgeType == "jpeg"
+    ) {
+      return null;
+    }
+  }
   return { InvalidType: true };
 }

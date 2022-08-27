44e6ef3161aa8f99d6218ae74f5ae2a545d9f098
Diego Otranto <diegui98@live.com.ar>
2022-08-27 01:47:01 -0300

    post method, empty "apiUrl"

--- src/app/core/services/publicApiService.service.ts
+++ src/app/core/services/publicApiService.service.ts
@@ -7,11 +7,17 @@ import { Observable } from 'rxjs';
 })
 export class PublicApiServiceService {
 
+  apiUrl!: string //Aqui iria la url de la api
+
   constructor(private http:HttpClient) { }
 
   get<T>(route:string, id?:number): Observable<T>{
 
     const url = route + (id ? '/'+id : '')//Falta la url base de la api al principio
     return this.http.get<T>(url) 
+  }
+
+  post(postUrl: string, obj: any): Observable<any> {
+    return this.http.post(this.apiUrl + postUrl, obj);
   }
 }

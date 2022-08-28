ee9dd62ab013f61c46a2b76091f0ee63376e50a0
GastionG <gastonmachado056@gmail.com>
2022-08-28 16:38:50 -0300

    metodo patch agregado

--- src/app/core/services/publicApiService.service.ts
+++ src/app/core/services/publicApiService.service.ts
@@ -1,17 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PrivateApiServiceService } from './privateApiService.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicApiServiceService {
 
-  constructor(private http:HttpClient) { }
+  apiUrl = '';
+  constructor(
+    private http:HttpClient,
+    private privateApiService: PrivateApiServiceService
+    ) { }
 
   get<T>(route:string, id?:number): Observable<T>{
 
     const url = route + (id ? '/'+id : '')//Falta la url base de la api al principio
     return this.http.get<T>(url) 
+  }
+
+  patch(data: any): void{ 
+    if(this.privateApiService.getHeaders()){
+      this.http.patch(this.apiUrl, data, {headers: this.privateApiService.getHeaders()})
+
+    }
   }
 }

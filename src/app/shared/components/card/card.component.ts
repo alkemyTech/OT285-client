9b2c03b53cbffd4eec25224c4800786b285d58fd
Nahuel Barreiro <nahuelbarreiro@gmail.com>
2022-08-28 12:43:44 -0300

    card styles changed

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -12,7 +12,7 @@ export class CardComponent implements OnInit {
     title:'Titulo',
     image:'LOGO-SOMOS MAS.png',
     description:'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <strong>Dolorum</strong> et veritatis rerum ipsam consequuntur, eum tenetur neque velit debitis minus eius fuga autem voluptatem earum quia, est suscipit! Odio, unde!</p>',
-    // imageLeft:true
+    imageLeft:true
   }
 
   constructor() { }

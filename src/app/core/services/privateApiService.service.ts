e11512ff6db0e769fb5a1094b696038d9b78531f
Lucio Cesar Cocuccio <lucio.cocuccio@i2t-sa.com.ar>
2022-08-29 08:41:04 -0300

    Metodo Get p/privateApiService

--- src/app/core/services/privateApiService.service.ts
+++ src/app/core/services/privateApiService.service.ts
@@ -1,21 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrivateApiServiceService {
+  
+  apiUrl: string = '.../'; //COMPLETAR
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getHeaders(): Headers | null {
+  getHeaders(): HttpHeaders | null { //Cambio Headers por HttpHeaders
     const token = localStorage.getItem('token')
     if(token){
-      const headers = new Headers({
+      const headers = new HttpHeaders({ //Cambio Headers por HttpHeaders
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       })
-      return headers
+      return headers;
     }
     return null
+  }
+
+  get<T>(route: string, id: number | null): Observable<T> {
+    const headers = this.getHeaders();
+    let url = this.apiUrl + route + (id ? '/' + id : '');
+    let apiCall: Observable<T>;
+    (headers ? apiCall = this.http.get<T>( url, { headers: headers }) : apiCall = this.http.get<T>(url));    
+    return apiCall;
   }
 }

e932aab7948ca62b19339a30ab6da3ee719ad86c
diegui98 <62922898+diegui98@users.noreply.github.com>
2022-08-29 10:06:44 -0300

    publicApiService post method (#25)
    * post method, empty "apiUrl"
    
    * +<T>
    
    * "postUrl" renamed to "url"

--- src/app/core/services/publicApiService.service.ts
+++ src/app/core/services/publicApiService.service.ts
@@ -7,11 +7,17 @@ import { Observable } from 'rxjs';
 })
 export class PublicApiServiceService {
 
+  apiUrl!: string //Aqui iria la url de la api
+
   constructor(private http:HttpClient) { }
 
   get<T>(route:string, id?:number): Observable<T>{
 
     const url = route + (id ? '/'+id : '')//Falta la url base de la api al principio
     return this.http.get<T>(url) 
+  }
+
+  post<T>(url: string, obj: any): Observable<T> {
+    return this.http.post<T>(this.apiUrl + url, obj);
   }
 }

a5a16d894d4f43dac31c0799a4454811ccc27d84
Gaston Machado <93017292+GastionG@users.noreply.github.com>
2022-08-29 10:05:36 -0300

    Create edit user form (#31)
    * Estilos de Titulo
    
    * Form categories added. Name and file validators.
    
    * creacion-contact
    
    * creacion-formulario-contact
    
    * formulario-finalizado
    
    * agregando-validaciones
    
    * creacion_form_actividades
    
    * agregando-funcionalidad-actiividades
    
    * Errors, image validator and creation-edition added
    
    * Return type de las funciones. min-height ckeditor en styles.scss
    
    * dashboard component, routing added (#4)
    
    * Agregada carpeta de validators en shared
    
    * Contact form (#5)
    
    * formulario-cracion-actividades-31
    
    * finalizando-form-edicion-creacion-actividades
    
    * cambios-botones
    
    * branch-contact-form
    
    * contact-form-38
    
    * contact-form-38
    
    * probando-validaciones
    
    * return type void corrected
    
    * org-data.html+css+ts, routing, image folder+logo (#8)
    
    * Login form (#3)
    
    * formulario login, diseño general
    
    * Incorporacion componente en rutas
    
    * Cambio div -> mat-error. Estilos en .scss general -> .scss componente
    
    * Elimino clase sin uso (alert). Mejora tipado (return:void)
    
    Co-authored-by: Lucio Cesar Cocuccio <lucio.cocuccio@i2t-sa.com.ar>
    
    * Register form (#2)
    
    * Formulario registro. Diseño gral, validaciones required, regEx pattern
    
    * Incorporacion componente en rutas
    
    * Estilos .scss gral -> .scss componente. Refactorizacion validator. Mejoras de tipado (return:void)
    
    Co-authored-by: Lucio Cesar Cocuccio <lucio.cocuccio@i2t-sa.com.ar>
    Co-authored-by: Carolina L <51510382+carolinalaborde@users.noreply.github.com>
    
    * Slides form (#7)
    
    * .ts: Form creation, Form getters
    
    * sendSlide Method, //get slideData and load form
    
    * slide-form Html
    
    * slide-form css
    
    * slides-form routing
    
    * global classes
    
    * cancel btn, classes fix
    
    * image input, cancel btn fix
    
    * loadImg Method
    
    * error messages
    
    * image+description classes fix
    
    * Order: text to number
    
    * Method Types, c.log(url)
    
    * Activity form 31 (#6)
    
    * formulario-cracion-actividades-31
    
    * finalizando-form-edicion-creacion-actividades
    
    * cambios-botones
    
    * add-activity-form
    
    * add-activity-form
    
    * activity-form-create
    
    * terminando-branch-activity
    
    * return type void corrected
    
    * resolviendo_conflictos_activity_form_31
    
    * Resolviendo-conflictos-final
    
    * pull-merge
    
    * Arreglando-conflictos-activity-form-31
    
    * Arreglando-conflictos-activity-form-31__2
    
    Co-authored-by: Carolina L <51510382+carolinalaborde@users.noreply.github.com>
    
    * cambio variable creationActivity
    
    * componente member eliminado
    
    * referencias al componente member borradas
    
    * carpeta models movida a la carpeta core y agregados return type a metodos
    
    * image validator de la carpeta shared en uso
    
    * user form agregado
    
    Co-authored-by: nahuelba <nahuelbarreiro@gmail.com>
    Co-authored-by: anjoalo1 <anjojalo1@gmail.com>
    Co-authored-by: Carolina L <51510382+carolinalaborde@users.noreply.github.com>
    Co-authored-by: Nahuel Barreiro <70066008+nahuelba@users.noreply.github.com>
    Co-authored-by: anjoalo1 <111457586+anjoalo1@users.noreply.github.com>
    Co-authored-by: diegui98 <62922898+diegui98@users.noreply.github.com>
    Co-authored-by: luccio93 <92583632+luccio93@users.noreply.github.com>
    Co-authored-by: Lucio Cesar Cocuccio <lucio.cocuccio@i2t-sa.com.ar>

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from "@angular/core";
 import { MatSelectModule } from '@angular/material/select';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatIconModule } from "@angular/material/icon";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
 import { MatTableModule } from "@angular/material/table";
 import { MatCardModule } from '@angular/material/card';
 

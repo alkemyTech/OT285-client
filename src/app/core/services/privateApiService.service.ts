03e9d57aec199b84263bc03386e1a57dcb2b4234
Diego Otranto <diegui98@live.com.ar>
2022-08-28 23:32:09 -0300

    put Method, apiUrl Property, HttpClient inject, "Headers" class in "getHeaders()" changed to HttpHeaders

--- src/app/core/services/privateApiService.service.ts
+++ src/app/core/services/privateApiService.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,16 +6,28 @@ import { Injectable } from '@angular/core';
 })
 export class PrivateApiServiceService {
 
-  constructor() { }
+  apiUrl!: string //Aca iria la url de la api
 
-  getHeaders(): Headers | null {
+  constructor(private http:HttpClient) { }
+
+  getHeaders(): HttpHeaders | null {
     const token = localStorage.getItem('token')
     if(token){
-      const headers = new Headers({
+      const headers = new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       })
       return headers
+    }
+    return null
+  }
+
+  put<T>(route: string, id: number, obj: any) {
+
+    const headers = this.getHeaders()
+    if (headers) {
+      let url = this.apiUrl + route + "/" + id
+      return this.http.put<T>( url, obj, { headers: headers } )
     }
     return null
   }

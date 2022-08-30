15444f56a5ca7018d66dbcbebfe513f851ccef15
anjoalo1 <anjojalo1@gmail.com>
2022-08-29 19:04:56 -0500

    screen-listado-slides(backoffice)-66

---
+++ src/app/features/pages/slides/slides-form/array.slide.ts
@@ -0,0 +1,13 @@
+export class listSlides{
+
+    constructor(title:string, image:string, order:string){
+        this.title = title;
+        this.image=image;
+        this.order=order;
+    }
+
+    title:string;
+    image:string;
+    order:string;
+
+}
--- src/app/features/pages/slides/slides-form/slides-form.component.ts
+++ src/app/features/pages/slides/slides-form/slides-form.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from "@angular/core";
+
 import {
   AbstractControl,
   FormBuilder,
   FormGroup,
   Validators,
 } from "@angular/forms";
+import { Router } from "@angular/router";
 import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { ImageValidator } from "src/app/shared/validators/image.validator";
+import { listSlides } from "./array.slide";
 
 @Component({
   selector: "app-slides-form",
   templateUrl: "./slides-form.component.html",
   styleUrls: ["./slides-form.component.scss"],
 })
+
+
+
+
 export class SlidesFormComponent implements OnInit {
   form: FormGroup;
   public Editor = ClassicEditor;
@@ -21,7 +28,8 @@ export class SlidesFormComponent implements OnInit {
   imageFile!: File | null;
 
   constructor(
-    private formBuilder: FormBuilder // Se inyecta => private http: HttpService
+    private formBuilder: FormBuilder, // Se inyecta => private http: HttpService
+    private routeCreate: Router //variable para redirigir /backoffice/Slides/create//OT285-66
   ) {
     this.form = this.formBuilder.group({
       name: ["", [Validators.required]],
@@ -61,12 +69,12 @@ export class SlidesFormComponent implements OnInit {
       order: this.order?.value,
     };
     if (this.slideEmpty) {
-      url = "/Slides/create";
+      //url = "/Slides/create";
       /*
       this.http.post(url, newForm).subscribe() 
       */
     } else if (!this.slideEmpty) {
-      url = "/Slides/" + "this.slideData.id";
+      //url = "/Slides/" + "this.slideData.id";
       /*
       this.http.patch(url, newForm).subscribe() 
       */
@@ -94,4 +102,47 @@ export class SlidesFormComponent implements OnInit {
   get order(): AbstractControl | null {
     return this.form.get("order");
   }
+
+
+
+
+   /* ************************************************************************* */
+  /**OT285-66 beginning */
+
+  mySlides:listSlides[]=[
+    new listSlides("title1", "./assets/images/LOGO-SOMOS MAS.png", "order1"),
+    new listSlides("title2", "./assets/images/LOGO-SOMOS MAS.png", "order2"),
+    new listSlides("title3", "./assets/images/LOGO-SOMOS MAS.png", "order3")
+  ];
+
+  deleteSlide(indice:number):void{
+
+    /**Este solo esta como modo de prueba
+     * para borrar los items del array
+     * borrar esta instruccion para borrar los slides 
+     * desde el servicio
+     */
+    this.mySlides.splice(indice, 1);
+
+  }
+
+
+  /** */
+  navigateCreate(): void{
+    /**En la sección superior, mostrará un boton que redirigirá a la ruta /backoffice/Slides/create */
+    this.routeCreate.navigate(['/backoffice/Slides']);
+
+  }
+/**OT285-66 end */
 }
+
+
+
+
+
+
+
+
+
+
+

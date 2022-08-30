3140b8a4873a621ec4d36240191e34f1adebd5d1
nahuelba <nahuelbarreiro@gmail.com>
2022-08-30 15:47:49 -0300

    Delete method added. return types

--- src/app/features/services/activities.service.ts
+++ src/app/features/services/activities.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { PrivateApiServiceService } from 'src/app/core/services/privateApiService.service';
 import { PublicApiServiceService } from 'src/app/core/services/publicApiService.service';
 
@@ -25,22 +26,23 @@ export class ActivitiesService {
     private PrivateApiService: PrivateApiServiceService
   ) { }
 
-  getActivities(){
+  getActivities(): Observable<any>{
     return this.PublicApiService.get('activities')
   }
 
-  getActivityById(id:number){
+  getActivityById(id:number): Observable<any>{
     return this.PublicApiService.get('activities', id)
   }
 
-  postActivity(Activity:Activity){
+  postActivity(Activity:Activity): Observable<any>{
     return this.PrivateApiService.post('activities', Activity)
   }
 
-  patchActivity(Activity:Activity, ActivityId:string){
-    // return this.PrivateApiService.patch('activities', Activity, ActivityId)
+  putActivity(Activity:Activity, ActivityId:number): Observable<any>{
+    return this.PrivateApiService.put('activities', ActivityId, Activity)
   }
 
-  deleteActivity(){
+  deleteActivity(ActivityId:number)/*: Observable<any>*/{
+    // return this.PrivateApiService.delete('activities', ActivityId)
   }
 }

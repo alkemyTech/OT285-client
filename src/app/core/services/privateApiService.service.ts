d35a4ceb211d319f2abbae6d4274be5b1e6e53ce
GastionG <gastonmachado056@gmail.com>
2022-08-29 22:52:01 -0300

    patch sin header

--- src/app/core/services/privateApiService.service.ts
+++ src/app/core/services/privateApiService.service.ts
@@ -1,42 +1,48 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PrivateApiServiceService {
-  
-  apiUrl: string = '.../';
+  apiUrl: string = ".../";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getHeaders(): HttpHeaders | null {
-    const token = localStorage.getItem('token')
-    if(token){
+    const token = localStorage.getItem("token");
+    if (token) {
       const headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      })
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      });
       return headers;
     }
-    return null
+    return null;
   }
 
   get<T>(route: string, id: number | null): Observable<T> {
     const headers = this.getHeaders();
-    const url = this.apiUrl + route + (id ? '/' + id : '');
+    const url = this.apiUrl + route + (id ? "/" + id : "");
+    
     let apiCall: Observable<T>;
-    (headers ? apiCall = this.http.get<T>( url, { headers: headers }) : apiCall = this.http.get<T>(url));    
+    headers
+      ? (apiCall = this.http.get<T>(url, { headers: headers }))
+      : (apiCall = this.http.get<T>(url));
+
     return apiCall;
   }
 
   patch<T>(route: string, id: number, data: {}): Observable<T> | null {
     const headers = this.getHeaders();
-    if (headers) {
-      const url = this.apiUrl + "/" + route + (id ? "/" + id : "");
-      return this.http.patch<T>(url, data, { headers: headers });
-    }
-    return null;
+    const url = this.apiUrl + "/" + route + (id ? "/" + id : "");
+
+    let apiCall: Observable<T>;
+    headers
+      ? (apiCall = this.http.patch<T>(url, data, { headers: headers }))
+      : (apiCall = this.http.patch<T>(url, data));
+
+    return apiCall;
   }
 }

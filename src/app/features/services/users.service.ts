7f0bad822b0913f8138562577182b25705afb3cf
Diego Otranto <diegui98@live.com.ar>
2022-08-30 15:58:52 -0300

    change to 5 methods, commented deleteUser

--- src/app/features/services/users.service.ts
+++ src/app/features/services/users.service.ts
@@ -31,100 +31,26 @@ export class UsersService {
 
   constructor(private privateApi: PrivateApiServiceService) {}
 
-  getAllUsers(
-    name?: string,
-    role?: string,
-    skip?: number,
-    limit?: number
-  ): any {
-    this.privateApi.get(this.route).subscribe(
-      (data) => {
-        //Lista Completa
-        let usersList: UsersList = data as UsersList;
-        //Busqueda por nombre
-        if (name) {
-          let usersListByName = [];
-          for (let i = 0; i < usersList.data.length; i++) {
-            if (usersList.data[i].name.includes(name)) {
-              usersListByName.push(usersList.data[i]);
-            }
-          }
-          usersList.data = usersListByName;
-        }
-        //Busqueda por Rol
-        if (role) {
-          let usersListByRole = [];
-          for (let i = 0; i < usersList.data.length; i++) {
-            if (usersList.data[i].role_id.includes(role)) {
-              usersListByRole.push(usersList.data[i]);
-            }
-          }
-          usersList.data = usersListByRole;
-        }
-        //Busqueda con Skip
-        if (skip) {
-          let usersListWithSkip = [];
-          usersListWithSkip = usersList.data.slice(skip);
-          usersList.data = usersListWithSkip;
-        }
-        //Busqueda con Limit
-        if (limit) {
-          let usersListWithLimit = [];
-          usersListWithLimit = usersList.data.slice(0, limit);
-          usersList.data = usersListWithLimit;
-        }
-        //Devuelve la lista modificada o no modificada
-        return usersList;
-      },
-      (err) => {
-        return err;
-      }
-    );
+  getAllUsers(): any {
+    return this.privateApi.get(this.route);
   }
 
   storeUser(obj: any): any {
-    this.privateApi.post(this.route, obj).subscribe(
-      (data) => {
-        return data;
-      },
-      (err) => {
-        return err;
-      }
-    );
+    return this.privateApi.post(this.route, obj);
   }
 
   getUser(id: number): any {
-    this.privateApi.get(this.route, id).subscribe(
-      (data) => {
-        return data;
-      },
-      (err) => {
-        return err;
-      }
-    );
+    return this.privateApi.get(this.route, id);
   }
 
   updateUser(id: number, obj: any): any {
-    let message: string;
-    this.privateApi.put(this.route, id, obj).subscribe(
-      (data) => {
-        return data;
-      },
-      (err) => {
-        return err;
-      }
-    );
+    return this.privateApi.put(this.route, id, obj);
   }
 
-  deleteUser(id: number): any {
+  /*
+    deleteUser(id: number): any {
     let message: string;
-    this.privateApi.delete(this.route, id).subscribe(
-      (data) => {
-        return data;
-      },
-      (err) => {
-        return err;
-      }
-    );
+     return this.privateApi.delete(this.route, id)
   }
+  */
 }

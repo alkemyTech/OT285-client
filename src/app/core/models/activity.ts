9d80c5febdd6568142ee4977b224750eba4a1653
Nahuel Barreiro <70066008+nahuelba@users.noreply.github.com>
2022-09-01 16:21:42 -0300

    OT285 64 Activities table (#50)
    * activities-list added
    
    * activity interface added in models
    
    * table of activities added

---
+++ src/app/core/models/activity.ts
@@ -0,0 +1,14 @@
+
+export interface Activity {
+    id: number;
+    name: string;
+    slug: string;
+    description: string;
+    image: string;
+    user_id: number;
+    category_id: number;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string;
+  }
+  

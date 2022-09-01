ccb1f972e52dc7b5bc485f7e83c01fad32839e9b
GastionG <gastonmachado056@gmail.com>
2022-09-01 04:47:56 -0300

    formulario funcional

--- src/app/features/pages/home/home-form/home-form.component.ts
+++ src/app/features/pages/home/home-form/home-form.component.ts
@@ -1,71 +1,75 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ImageValidator } from 'src/app/shared/validators/image.validator';
-import { UrlValidator } from 'src/app/shared/validators/url.validator';
+import { Component, Input, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { ImageValidator } from "src/app/shared/validators/image.validator";
 
 @Component({
-  selector: 'app-home-form',
-  templateUrl: './home-form.component.html',
-  styleUrls: ['./home-form.component.scss']
+  selector: "app-home-form",
+  templateUrl: "./home-form.component.html",
+  styleUrls: ["./home-form.component.scss"],
 })
 export class HomeFormComponent implements OnInit {
-  slide = {
-    text: 'aaaaaa',
-    image: 'assets/images/LOGO-SOMOS MAS.png'
-  };
   @Input() home = {
-    welcomeText: '',
+    welcomeText: "This is a welcome text",
     slides: [
-      this.slide,
-      this.slide,
-      this.slide
-    ]
-  };
-  form = this.fb.group({
-    welcomeText: ["", [Validators.required, Validators.minLength(20)]],
-    slides: [
-      [],
       {
-        validators: [Validators.required],
-        updateOn: "submit",
+        text: "a1",
+        image: "",
+      },
+      {
+        text: "a2",
+        image: "",
+      },
+      {
+        text: "a3",
+        image: "",
       },
     ],
+  };
+
+  form = this.fb.group({
+    welcomeText: ["", [Validators.required, Validators.minLength(20)]],
+    slides: this.fb.array([]),
   });
-  fileName = "";
-  
-  constructor(
-    private fb: FormBuilder
-  ) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    if (this.home) {
+      this.form.controls.welcomeText.setValue(this.home.welcomeText);
+      for (let i = 0; i < this.home.slides.length; i++) {
+        const slide = this.home.slides[i];
+        const slideModel = this.fb.group({
+          text: [slide.text, Validators.required],
+          image: [slide.image, [Validators.required, ImageValidator]],
+        });
+        this.slides.push(slideModel);
+      }
+    }
+  }
 
-  }
-  addSlide(event: any): void{
-    const slide = event.value;
-    if (slide != "") {
-      this.uploadSlides(slide);
-    }
-  }
-  uploadSlides(slide: any): void{
-    const slides: [any] = this.form.get("slides")?.value;
-    if(slide){
-      slides.push(slide);
-      this.form.controls["links"].setValue(slides);
-    }
-  }
-  fileChangeEvent(event: any){
+  fileChangeEvent(event: any, slide: FormGroup): void {
     const file: File = event.target.files[0];
 
     if (file) {
-      this.fileName = file.name;
-      this.form.controls["image"].setValue(file);
+      slide.controls.image.setValue(file);
     }
   }
-  send(): void{
-
+  send(): void {
+    console.log("actualizar");
+    console.log(this.form.value);
   }
-  cancel(): void{
-
+  cancel(): void {
+    console.log("cancelar");
   }
-  
+  toGroup(abs: AbstractControl): FormGroup {
+    return abs as FormGroup;
+  }
+  get slides() {
+    return this.form.controls["slides"] as FormArray;
+  }
 }

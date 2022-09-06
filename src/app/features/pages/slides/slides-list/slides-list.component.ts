108cf2b145969595a7fe80c2efc17f0e1d244eba
diegui98 <62922898+diegui98@users.noreply.github.com>
2022-09-06 15:00:28 -0300

    Ot285 112 reducer slides (#61)
    * new state/ actions,effects,reducer
    slides-list.ts slidesList replaced with ngrx
    slides-list.html added async to table [datasource]
    new interface slide
    
    * added type any to fix a 'unknown type' error
    
    * createSlide renamed to goToCreateSlide
    removed some console.logs

--- src/app/features/pages/slides/slides-list/slides-list.component.ts
+++ src/app/features/pages/slides/slides-list/slides-list.component.ts
@@ -1,66 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { getError, getSlides, State } from "../state/slides.reducer";
+import * as SlidesActions from "../state/slides.actions";
+import { Slide } from "src/app/core/models/slide";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-slides-list',
-  templateUrl: './slides-list.component.html',
-  styleUrls: ['./slides-list.component.scss']
+  selector: "app-slides-list",
+  templateUrl: "./slides-list.component.html",
+  styleUrls: ["./slides-list.component.scss"],
 })
 export class SlidesListComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private store: Store<State>) {}
 
   ngOnInit(): void {
+    this.slidesList$ = this.store.select(getSlides);
+    this.errorMessage$ = this.store.select(getError);
+    this.store.dispatch(SlidesActions.loadSlides());
   }
 
-  slidesList = [
-    {
-      title: 'title1',
-      image: 'assets/images/LOGO-SOMOS MAS.png',
-      order: 'order 1'
-    },
+  errorMessage$!: Observable<string>;
+  slidesList$!: Observable<Slide[]>;
 
-    {
-      title: 'title2',
-      image: 'assets/images/LOGO-SOMOS MAS.png',
-      order: 'order 2'
-    },
-
-    {
-      title: 'title 3',
-      image: 'assets/images/LOGO-SOMOS MAS.png',
-      order: 'order 3'
-    },
-    
-    
-  ];
   columns = ["Title", "Image", "Order", "Editar/Eliminar"];
 
   deleting = false;
   userFlag = {};
 
-
   editListSlide(): void {
-    console.log('edit'); 
+    console.log("edit");
   }
   deleteListSlide(user: {}): void {
     this.userFlag = user;
     this.deleting = true;
-
   }
-  trueDelete(): void{
-    console.log('peticion para eliminar el usuario en el servidor');
+  trueDelete(): void {
     this.deleting = false;
-    this.userFlag = {}
+    this.userFlag = {};
   }
-  cancel(): void{
-    console.log('cancelada la eliminacion del usuario');
+  cancel(): void {
     this.deleting = false;
-    this.userFlag = {}
+    this.userFlag = {};
   }
-  createSlide(): void {
-    console.log('redireccion a backoffice/slides/create');
-    this.router.navigate(['backoffice/slides/create']);
+  goToCreateSlide(): void {
+    this.router.navigate(["backoffice/slides/create"]);
   }
-
 }

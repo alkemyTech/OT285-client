0251a5836c8d8e81982526dbf16efdbaac759b86
nahuelba <nahuelbarreiro@gmail.com>
2022-09-08 11:19:24 -0300

    users effect and store initialization

--- src/app/features/pages/users/users.module.ts
+++ src/app/features/pages/users/users.module.ts
@@ -5,10 +5,20 @@ import { UsersRoutingModule } from "./users-routing.module";
 import { UserFormComponent } from "./user-form/user-form.component";
 import { UsersListComponent } from "./users-list/users-list.component";
 import { SharedModule } from "src/app/shared/shared.module";
+import { StoreModule } from "@ngrx/store";
+import { usersReducer } from "./state/users.reducer";
+import { UserEffects } from "./state/users.effects";
+import { EffectsModule } from "@ngrx/effects";
 
 @NgModule({
   declarations: [UsersListComponent, UserFormComponent],
-  imports: [CommonModule, UsersRoutingModule, SharedModule],
+  imports: [
+    CommonModule, 
+    UsersRoutingModule, 
+    SharedModule,
+    StoreModule.forFeature('users',usersReducer),
+    EffectsModule.forFeature([UserEffects])
+  ],
   exports: [UsersListComponent, UserFormComponent],
 })
 export class UsersModule {}

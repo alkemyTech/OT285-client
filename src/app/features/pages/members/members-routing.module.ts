9c5941d5222e5a8ff9cee2ebc343e874dd4f4bea
nahuelba <nahuelbarreiro@gmail.com>
2022-09-08 14:41:31 -0300

    set storemodule and effectsmodule in members module

--- src/app/features/pages/members/members-routing.module.ts
+++ src/app/features/pages/members/members-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MemberFormComponent } from './member-form/member-form.component';
+import { MemberslistComponent } from './members-list/members-list.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component:MemberslistComponent
+  },
   {
     path:'create',
     component:MemberFormComponent
--- src/app/features/pages/members/members.module.ts
+++ src/app/features/pages/members/members.module.ts
@@ -4,17 +4,25 @@ import { CommonModule } from '@angular/common';
 import { MembersRoutingModule } from './members-routing.module';
 import { MemberFormComponent } from './member-form/member-form.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { MembersReducer } from './state/members.reducers';
+import { MembersEffects } from './state/members.effects';
+import { MemberslistComponent } from './members-list/members-list.component';
 
 
 @NgModule({
   declarations: [
     MemberFormComponent,
+    MemberslistComponent
 
   ],
   imports: [
     CommonModule,
     MembersRoutingModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forFeature('members',MembersReducer),
+    EffectsModule.forFeature([MembersEffects])
   ]
 })
 export class MembersModule { }

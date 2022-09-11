31744fbe38081491813a1a9af6a837bc9a21d39f
Diego Otranto <diegui98@live.com.ar>
2022-09-11 19:45:27 -0300

    added material example Dialog new html terms-of-service

--- src/app/features/pages/auth/auth.module.ts
+++ src/app/features/pages/auth/auth.module.ts
@@ -1,22 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-
+import { AuthRoutingModule } from "./auth-routing.module";
+import { LoginFormComponent } from "./login-form/login-form.component";
+import {
+  RegisterFormComponent,
+  TermsOfServiceComponent,
+} from "./register-form/register-form.component";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
   declarations: [
     LoginFormComponent,
     RegisterFormComponent,
+    TermsOfServiceComponent,
   ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    SharedModule,
-
-  ]
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
-export class AuthModule { }
+export class AuthModule {}

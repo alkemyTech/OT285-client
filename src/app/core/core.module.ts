62963e43ac79d30742df8059ceeb7a01dd8ab791
Diego Otranto <diegui98@live.com.ar>
2022-09-11 18:19:02 -0300

    href to routerLink, RouterModule added to core.module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HttpService } from "./services/http.service";
 
 @NgModule({
   declarations: [SidebarComponent],
-  imports: [CommonModule, HttpClientModule, SharedModule],
+  imports: [CommonModule, HttpClientModule, SharedModule, RouterModule],
   providers: [HttpService],
 })
 export class CoreModule {}

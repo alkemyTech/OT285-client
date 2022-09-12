7cd8a4193d863e93ca9610892053f9015a040b30
Gaston Machado <93017292+GastionG@users.noreply.github.com>
2022-09-12 12:10:59 -0300

    loading agregado a la tabla (#65)
    * loading agregado a la tabla
    
    * progress component reubicado en core
    
    * progress renombrado a progress-bar
    
    * loader con interceptor creado
    
    * position del progress cambiado a fixed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
@@ -7,9 +7,12 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HttpService } from "./services/http.service";
 import { PublicHeaderComponent } from "./components/public-header/public-header.component";
 import { MaterialModule } from "../shared/material.module";
+import { ProgressLoaderComponent } from "./components/progress-loader/progress-loader.component";
+import { LoaderService } from "./components/progress-loader/loader.service";
+import { ProgressLoaderInterceptorService } from "./interceptor/progress-loader-interceptor.service";
 
 @NgModule({
-  declarations: [PublicHeaderComponent, SidebarComponent],
+  declarations: [PublicHeaderComponent, SidebarComponent, ProgressLoaderComponent],
   imports: [
     CommonModule,
     HttpClientModule,
@@ -17,7 +20,15 @@ import { MaterialModule } from "../shared/material.module";
     RouterModule,
     SharedModule,
   ],
-  providers: [HttpService],
-  exports: [PublicHeaderComponent],
+  providers: [
+    HttpService,
+    LoaderService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ProgressLoaderInterceptorService,
+      multi: true,
+    }
+  ],
+  exports: [PublicHeaderComponent, ProgressLoaderComponent],
 })
 export class CoreModule {}

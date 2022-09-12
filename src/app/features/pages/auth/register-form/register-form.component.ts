9eac371c3ba45290474e2c09817b20c012b9d710
diegui98 <62922898+diegui98@users.noreply.github.com>
2022-09-12 15:44:46 -0300

    Ot285 123 terminos y condiciones (#72)
    * import MatDialogModule in material.module
    
    * added material example Dialog
    new html terms-of-service
    
    * Dialog feedback if canceled or acepted
    "Enviar" button disabled if Dialog Canceled
    new import MatCheckboxModule in material.module
    
    * better button, added error feedback
    
    * fix button type=button
    
    * camelCase Fix, openDialog return void Fix
    terms-of-service moved to new folder

--- src/app/features/pages/auth/register-form/register-form.component.ts
+++ src/app/features/pages/auth/register-form/register-form.component.ts
@@ -1,42 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MustMatch } from 'src/app/shared/validators/pass-match.validator';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { MustMatch } from "src/app/shared/validators/pass-match.validator";
+import { TermsOfServiceComponent } from "./terms-of-service/terms-of-service.component";
 
 @Component({
-  selector: 'app-register-form',
-  templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  selector: "app-register-form",
+  templateUrl: "./register-form.component.html",
+  styleUrls: ["./register-form.component.scss"],
 })
-
 export class RegisterFormComponent implements OnInit {
-  
   registerForm!: FormGroup;
+  termsOfServiceResult!: boolean;
+  termsOfServiceTouched: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private dialog: MatDialog) {}
 
-  ngOnInit(): void { 
-    this.registerForm = this.formBuilder.group({      
-      email: ['', [Validators.required, Validators.email, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,4}')]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('(?=[^a-zA-Z]*[a-zA-Z])(?=[^0-9]*[0-9])(?=.*[@$!%*#?&]).{3,}') ]],
-      confirmPassword: ['', Validators.required],
-  }, {
-      validator: MustMatch('password', 'confirmPassword')
-  });
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        email: [
+          "",
+          [
+            Validators.required,
+            Validators.email,
+            Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,4}"),
+          ],
+        ],
+        password: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(
+              "(?=[^a-zA-Z]*[a-zA-Z])(?=[^0-9]*[0-9])(?=.*[@$!%*#?&]).{3,}"
+            ),
+          ],
+        ],
+        confirmPassword: ["", Validators.required],
+      },
+      {
+        validator: MustMatch("password", "confirmPassword"),
+      }
+    );
   }
 
   // getter para rapido acceso a form fields
-  get f() { return this.registerForm.controls; }
+  get f() {
+    return this.registerForm.controls;
+  }
 
-  register(): void{
+  register(): void {
     console.log(this.registerForm);
 
     // return si formulario es invalido
     if (this.registerForm.invalid) {
-        return;
+      return;
     }
   }
 
   onReset(): void {
     this.registerForm.reset();
+    this.termsOfServiceResult = false;
+    this.termsOfServiceTouched = false;
+  }
+
+  openDialog(): void {
+    this.termsOfServiceTouched = true;
+    const dialogRef = this.dialog.open(TermsOfServiceComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == undefined) {
+        result = false;
+      }
+      this.termsOfServiceResult = result;
+    });
   }
 }

181ce86746c7d09a94b1d116cf9e824e31230114
Lucio Cesar Cocuccio <lucioc93@gmail.com>
2022-09-12 12:37:27 -0300

    fix merge

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from "@angular/common";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
-import { FooterComponent } from "./components/footer/footer.component";
 import { HttpService } from "./services/http.service";
 import { PublicHeaderComponent } from "./components/public-header/public-header.component";
 import { MaterialModule } from "../shared/material.module";
@@ -11,11 +10,13 @@ import { LoaderService } from "./components/progress-loader/loader.service";
 import { ProgressLoaderInterceptorService } from "./interceptor/progress-loader-interceptor.service";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { RouterModule } from "@angular/router";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
   declarations: [
     PublicHeaderComponent, 
-    SidebarComponent, ProgressLoaderComponent,
+    SidebarComponent, 
+    ProgressLoaderComponent,
     FooterComponent
   ],
   imports: [    
@@ -27,8 +28,17 @@ import { RouterModule } from "@angular/router";
   ],
   exports: [
     FooterComponent,
-    PublicHeaderComponent
+    PublicHeaderComponent,
+    ProgressLoaderComponent
   ],
-  providers: [HttpService],
+  providers: [
+    HttpService,
+    LoaderService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ProgressLoaderInterceptorService,
+      multi: true,
+    }
+  ],
 })
 export class CoreModule {}
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,10 +2,8 @@ import { RouterModule } from "@angular/router";
 import { CommonModule, CurrencyPipe } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
-
 import { SharedModule } from "../shared/shared.module";
 import { HomepageComponent } from "./pages/homepage/homepage.component";
-import { CoreModule } from "../core/core.module";
 import { authReducer } from "./pages/auth/state/auth.reducers";
 import { AuthEffects } from "./pages/auth/state/auth.effects";
 import { EffectsModule } from "@ngrx/effects";

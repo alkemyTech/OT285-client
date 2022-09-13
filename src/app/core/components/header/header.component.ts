bbfdcd3ac310de3cb39bfc99119f342c82bc52db
nahuelba <nahuelbarreiro@gmail.com>
2022-09-13 18:34:33 -0300

    sidebar functionality added

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+
+  @Output() OpenCloseSidebar = new EventEmitter();
 
   items = [
     {
@@ -32,6 +35,10 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSidebar(){
+    this.OpenCloseSidebar.emit()
   }
 
 

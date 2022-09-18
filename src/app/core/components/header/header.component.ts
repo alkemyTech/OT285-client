26511f4b1ce0b3cb80b1e4a71e98294d479af129
diegui98 <diegui98@live.com.ar>
2022-09-18 01:52:19 -0300

    header is hides logout btn if not logged

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserInfo } from 'src/app/features/services/auth.service';
+import { State } from 'src/app/state/app.state';
+import { Store } from '@ngrx/store';
+import { getUser } from 'src/app/features/pages/auth/state/auth.reducers';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +11,16 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  logged$!: Observable<UserInfo | null>;
 
   @Output() OpenCloseSidebar = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private store:Store<State>
+  ) { }
 
   ngOnInit(): void {
+    this.logged$ = this.store.select(getUser) //check if user is logged
   }
 
   onSidebar() : void{

f5aec4798e2dccd16b45ed31685b19729bdeb924
Gaston Machado <93017292+GastionG@users.noreply.github.com>
2022-09-19 17:12:07 -0300

    Ot285 120 mercadopago donation (#83)
    * componente donation refactorizado
    
    * logo de mercadopago actualiazdo

--- src/app/features/pages/donations/donation/donation.component.ts
+++ src/app/features/pages/donations/donation/donation.component.ts
@@ -1,42 +1,19 @@
-import { CurrencyPipe } from "@angular/common";
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { Router } from "@angular/router";
-import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-donation",
   templateUrl: "./donation.component.html",
   styleUrls: ["./donation.component.scss"],
 })
-export class DonationComponent implements OnInit, OnDestroy {
+export class DonationComponent implements OnInit {
   @Input() text = "¡Contribuye!";
-  cant = new FormControl();
-  noDigit = new RegExp(/\D/g);
-  firstZeros = new RegExp(/^0+/);
 
-  cantValueChange = new Subscription();
-
-  constructor(private router: Router, private currencyPipe: CurrencyPipe) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.cantValueChange = this.cant.valueChanges.subscribe((val) => {
-      const value = val.replace(this.noDigit, "").replace(this.firstZeros, "");
-      this.cant.setValue(
-        this.currencyPipe.transform(value, "USD", "symbol", "1.0-0"),
-        { emitEvent: false }
-      );
-    });
   }
 
   send(): void {
-    console.log(this.cant.value);
-    if (this.cant.value) {
-      this.router.navigate(["gracias"]);
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.cantValueChange.unsubscribe();
+    window.open('https://link.mercadopago.com.ar/alkemy285', '_blank')
   }
 }

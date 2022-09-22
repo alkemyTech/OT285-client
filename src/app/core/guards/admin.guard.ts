47819200b6760298bf3817ad164384658bdbbc1c
GastionG <gastonmachado056@gmail.com>
2022-09-22 13:58:39 -0300

    admin guard terminado

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -1,5 +1,4 @@
-import { Injectable, OnDestroy, OnInit } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
@@ -7,21 +6,15 @@ import {
   UrlTree,
 } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { Observable, of, Subscription } from "rxjs";
-import { map, tap, catchError } from "rxjs/operators";
-import { getAuthenticationData } from "src/app/features/pages/auth/state/actions/auth-page.actions";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { AuthState, getUser } from "src/app/features/pages/auth/state/auth.reducers";
-import { AuthService, UserInfo } from "src/app/features/services/auth.service";
 
 @Injectable({
   providedIn: "root",
 })
-export class AdminGuard implements CanActivate, OnInit {
-  user$!:Observable<UserInfo | null>; 
-  userSub: Subscription = new Subscription;
-  val = false;
-
-  constructor(private authService: AuthService, private store:Store<AuthState>) {}
+export class AdminGuard implements CanActivate{
+  constructor(private store:Store<AuthState>) {}
   ngOnInit(): void {
 
   }
@@ -34,36 +27,17 @@ export class AdminGuard implements CanActivate, OnInit {
     | boolean
     | UrlTree 
   {
-    // return this.store.select(getUser).pipe(map(res => {
-    //   console.log(res);
-    //   if(res?.admin){
-    //     console.log(true);
-    //     return true;
-    //   }
-    //   else{
-    //     console.log(false);
-    //     return false;
-    //   }
-    // }))
-
-    this.user$ = this.store.select(getUser);
-
-    this.store.select(getUser).subscribe(res => {
+    return this.store.select(getUser).pipe(map(res => {
       console.log(res);
-      if(res){
-        if(res?.admin){
-          console.log(true);
-          this.val = true
-        }
-        else{
-          console.log(false);
-          this.val = false
-        }
+      if(res?.admin){
+        console.log('adminguard acepta');
+        return true;
       }
-    })
-    console.log('val', this.val);
-    
-    return this.val;
+      else{
+        console.log('adminguard rechaza');
+        return false;
+      }
+    }));
   }
 
 }
